ad86dd3c3d7fc1f89646e88d5e60deb1ac572631
Jiahao XU <Jiahao_XU@outlook.com>
2021-09-14 18:49:22 +1000

    Simplify EstablishedSession::wait impl by creating wait_impl
    which uses `?` to auto propagate error.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/connection/session.rs
+++ src/connection/session.rs
@@ -8,43 +8,48 @@ pub struct EstablishedSession {
     pub(super) session_id: u32,
 }
 impl EstablishedSession {
+    fn check_session_id(&self, session_id: u32) -> Result<()> {
+        if self.session_id != session_id {
+            Err(Error::UnmatchedSessionId)
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Return None if TtyAllocFail, Some(exit_value) if the process exited.
+    async fn wait_impl(&mut self) -> Result<Option<u32>> {
+        use Response::*;
+
+        match self.conn.read_response().await? {
+            TtyAllocFail { session_id } => {
+                self.check_session_id(session_id)?;
+                Result::Ok(None)
+            },
+            ExitMessage { session_id, exit_value } => {
+                self.check_session_id(session_id)?;
+                Result::Ok(Some(exit_value))
+            },
+            response =>
+                Err(Error::InvalidServerResponse(
+                    "Expected Response TtyAllocFail or ExitMessage",
+                    response
+                )),
+        }
+    }
+
     /// Wait for session status to change
     ///
     /// Return `Self` on error so that you can handle the error and restart
     /// the operation.
     pub async fn wait(mut self) -> Result<SessionStatus, (Error, Self)> {
-        use Response::*;
-
-        let response = match self.conn.read_response().await {
-            Result::Ok(response) => response,
-            Err(err) => return Err((err, self)),
-        };
-
-        match response {
-            TtyAllocFail { session_id } => {
-                if self.session_id != session_id {
-                    Err((Error::UnmatchedSessionId, self))
-                } else {
-                    Result::Ok(SessionStatus::TtyAllocFail(self))
-                }
-            },
-            ExitMessage { session_id, exit_value } => {
-                if self.session_id != session_id {
-                    Err((Error::UnmatchedSessionId, self))
-                } else {
-                    Result::Ok(SessionStatus::Exited {
-                        conn: self.conn,
-                        exit_value,
-                    })
-                }
-            },
-            response => Err((
-                Error::InvalidServerResponse(
-                    "Expected Response TtyAllocFail or ExitMessage",
-                    response
-                ),
-                self
-            ))
+        match self.wait_impl().await {
+            Ok(Some(exit_value)) =>
+                Ok(SessionStatus::Exited {
+                    conn: self.conn,
+                    exit_value,
+                }),
+            Ok(None) => Ok(SessionStatus::TtyAllocFail(self)),
+            Err(err) => Err((err, self)),
         }
     }
 }
