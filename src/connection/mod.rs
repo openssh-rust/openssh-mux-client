992506e8c0bf066244010484e450a83f4f3c9fce
Jiahao XU <Jiahao_XU@outlook.com>
2021-09-14 14:08:50 +1000

    Impl EstablishedSession::wait and Add new enum SessionStatus
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -159,3 +159,42 @@ pub struct EstablishedSession {
     conn: Connection,
     session_id: u32,
 }
+impl EstablishedSession {
+    fn check_session_id(&self, session_id: u32) -> Result<()> {
+        if self.session_id != session_id {
+            Err(Error::UnmatchedSessionId)
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Wait for session status to change
+    pub async fn wait(mut self) -> Result<SessionStatus> {
+        use Response::*;
+
+        match self.conn.read_response().await? {
+            TtyAllocFail { session_id } => {
+                self.check_session_id(session_id)?;
+                Result::Ok(SessionStatus::TtyAllocFail(self))
+            },
+            ExitMessage { session_id, exit_value } => {
+                self.check_session_id(session_id)?;
+                Result::Ok(SessionStatus::Exited {
+                    conn: self.conn,
+                    exit_value,
+                })
+            },
+            _ => Err(Error::InvalidServerResponse(
+                "Expected Response TtyAllocFail or ExitMessage"
+            ))
+        }
+    }
+}
+
+pub enum SessionStatus {
+    TtyAllocFail(EstablishedSession),
+    Exited {
+        conn: Connection,
+        exit_value: u32,
+    },
+}
