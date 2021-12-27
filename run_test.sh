fba8cd059c0d6388f0a201aa68d8475b2905c798
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-27 13:59:24 +1100

    Verify that ssh stop listening in run_test.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+ControlPath=/tmp/openssh-mux-client-test.socket
+
 stop_ssh_tester() {
     exit_code=$?
     testfiles/stop.sh
@@ -28,8 +30,18 @@ if [ $# -lt 1 ]; then
     cargo test test_unordered -- --nocapture
     cargo test test_request_stop_listening -- --nocapture
 
+    if [ -e $ControlPath ]; then
+        echo request_stop_listening does not work
+        exit 1
+    fi
+
     start_ssh_tester
     cargo test test_sync_request_stop_listening -- --nocapture
+
+    if [ -e $ControlPath ]; then
+        echo shutdown_mux_master does not work
+        exit 1
+    fi
 else
     cargo test $@ -- --nocapture
 fi
