dab27b3ef64399176c293624278fd450d5ac05fb
Jiahao XU <Jiahao_XU@outlook.com>
2022-01-03 17:05:38 +1100

    Fix default_code for `Session::term`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/request.rs
+++ src/request.rs
@@ -120,7 +120,7 @@ pub struct Session<'a> {
     pub escape_ch: char,
 
     /// Generally set to `$TERM`.
-    #[builder(default_code = r#"default_config::get_term().into()"#)]
+    #[builder(default_code = r#"Cow::Borrowed(default_config::get_term().into())"#)]
     pub term: Cow<'a, NonZeroByteSlice>,
     pub cmd: Cow<'a, NonZeroByteSlice>,
 }
