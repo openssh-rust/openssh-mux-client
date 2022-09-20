402e8465a2634dc82dfbb2a2ea74baf29e82ba70
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-20 21:59:26 +1000

    Refactor: Mv current root crate to crates/mux-client
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_test.sh
+++ run_test.sh
@@ -22,32 +22,38 @@ start_ssh_tester() {
     fi
 }
 
+test_mux_client() {
+    cd crates/mux-client
+
+    cargo +nightly miri test non_zero_bytes
+
+    ../../start_ssh_tester
+
+    if [ $# -lt 1 ]; then
+        cargo test test_unordered -- --nocapture
+        cargo test test_request_stop_listening -- --nocapture
+
+        if [ -e $ControlPath ]; then
+            echo request_stop_listening does not work
+            exit 1
+        fi
+
+        ../../start_ssh_tester
+        cargo test test_sync_request_stop_listening -- --nocapture
+
+        if [ -e $ControlPath ]; then
+            echo shutdown_mux_master does not work
+            exit 1
+        fi
+    else
+        cargo test "$@" -- --nocapture
+    fi
+}
+
 cd "$(dirname "$(realpath "$0")")"
 
 trap stop_ssh_tester 0
 
 testfiles/start.sh
 
-cargo +nightly miri test non_zero_bytes
-
-start_ssh_tester
-
-if [ $# -lt 1 ]; then
-    cargo test test_unordered -- --nocapture
-    cargo test test_request_stop_listening -- --nocapture
-
-    if [ -e $ControlPath ]; then
-        echo request_stop_listening does not work
-        exit 1
-    fi
-
-    start_ssh_tester
-    cargo test test_sync_request_stop_listening -- --nocapture
-
-    if [ -e $ControlPath ]; then
-        echo shutdown_mux_master does not work
-        exit 1
-    fi
-else
-    cargo test "$@" -- --nocapture
-fi
+test_mux_client "$@"
