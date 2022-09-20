e6e6f77ed4e8a422d11d9d93bf0eaa7eaf5b21f5
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-20 22:12:54 +1000

    Refactor: Mv current root crate to crates/mux-client (#11)
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_test.sh
+++ run_test.sh
@@ -2,52 +2,58 @@
 
 set -euxo pipefail
 
+project_dir="$(dirname "$(realpath "$0")")"
+
 ControlPath=/tmp/openssh-mux-client-test.socket
 
 stop_ssh_tester() {
     exit_code=$?
-    testfiles/stop.sh
+    "$project_dir"/testfiles/stop.sh
     exit $exit_code
 }
 
 start_ssh_tester() {
-    testfiles/start_ssh.sh
+    "$project_dir"/testfiles/start_ssh.sh
     
-    ControlMasterPID="$(testfiles/get_control_master_pid.sh)"
+    ControlMasterPID="$("$project_dir"/testfiles/get_control_master_pid.sh)"
     export ControlMasterPID
     if [ -z "$ControlMasterPID" ]; then
         echo Failed to start ssh
-        cat testfiles/*log
+        cat "$project_dir"/testfiles/*log
         exit 1
     fi
 }
 
-cd "$(dirname "$(realpath "$0")")"
+test_mux_client() {
+    cd "$project_dir"/crates/mux-client
+
+    cargo +nightly miri test non_zero_bytes
+
+    start_ssh_tester
+
+    if [ $# -lt 1 ]; then
+        cargo test test_unordered -- --nocapture
+        cargo test test_request_stop_listening -- --nocapture
+
+        if [ -e $ControlPath ]; then
+            echo request_stop_listening does not work
+            exit 1
+        fi
+
+        start_ssh_tester
+        cargo test test_sync_request_stop_listening -- --nocapture
+
+        if [ -e $ControlPath ]; then
+            echo shutdown_mux_master does not work
+            exit 1
+        fi
+    else
+        cargo test "$@" -- --nocapture
+    fi
+}
 
 trap stop_ssh_tester 0
 
-testfiles/start.sh
+"$project_dir"/testfiles/start.sh
 
-cargo +nightly miri test non_zero_bytes
-
-start_ssh_tester
-
-if [ $# -lt 1 ]; then
-    cargo test test_unordered -- --nocapture
-    cargo test test_request_stop_listening -- --nocapture
-
-    if [ -e $ControlPath ]; then
-        echo request_stop_listening does not work
-        exit 1
-    fi
-
-    start_ssh_tester
-    cargo test test_sync_request_stop_listening -- --nocapture
-
-    if [ -e $ControlPath ]; then
-        echo shutdown_mux_master does not work
-        exit 1
-    fi
-else
-    cargo test "$@" -- --nocapture
-fi
+test_mux_client "$@"
