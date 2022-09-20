f326d9a4c2baf2bbe76dfc55387791e0e260fc2d
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-20 17:11:26 +1000

    Impl new enum `utils::MaybeOwned`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/utils.rs
+++ src/utils.rs
@@ -2,3 +2,19 @@
 pub(crate) fn serialize_u32(int: u32) -> [u8; 4] {
     int.to_be_bytes()
 }
+
+pub(crate) enum MaybeOwned<'a, T> {
+    Owned(T),
+    Borrowed(&'a T),
+}
+
+impl<T> MaybeOwned<'_, T> {
+    pub(crate) fn as_ref(&self) -> &T {
+        use MaybeOwned::*;
+
+        match self {
+            Owned(val) => val,
+            Borrowed(reference) => reference,
+        }
+    }
+}
