b216b3b4caf67c04f84c6738ad4e0f053138451d
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-21 16:37:38 +1000

    Add new enum `Error`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ crates/proxy-client/src/error.rs
@@ -0,0 +1,31 @@
+use std::io;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    /// Error when waiting for response
+    #[error("Error when waiting for response: {0}.")]
+    AwaitableError(#[from] awaitable::Error),
+
+    /// IO Error (Excluding `io::ErrorKind::EWOULDBLOCK`).
+    #[error("IO Error: {0}.")]
+    IOError(#[from] io::Error),
+
+    /// Failed to serialize/deserialize the message: {0}.
+    #[error("Failed to serialize/deserialize the message: {0}.")]
+    FormatError(#[from] ssh_format::Error),
+
+    /// The response id is invalid.
+    #[error("The response id {response_id} is invalid.")]
+    InvalidResponseId {
+        /// The invalid response id
+        response_id: u32,
+    },
+
+    /// Invalid response from the sftp-server
+    #[error("Response from sftp server is invalid: {0}")]
+    InvalidResponse(
+        // Use `&&str` since `&str` takes 16 bytes while `&str` only takes 8 bytes.
+        &'static &'static str,
+    ),
+}
--- crates/proxy-client/src/lib.rs
+++ crates/proxy-client/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod error;
+pub use error::Error;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
