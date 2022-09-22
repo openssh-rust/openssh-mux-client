9087ae02d484b336a64691e2add5c1281ad041ee
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-23 00:24:50 +1000

    Use `CompactString` for str in response & temporarily remove `data`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/response/channel/mod.rs
+++ crates/proxy-client/src/response/channel/mod.rs
@@ -1,4 +1,5 @@
 use bytes::Bytes;
+use compact_str::CompactString;
 use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize)]
@@ -7,15 +8,14 @@ pub(crate) struct OpenConfirmation {
     sender_channel: u32,
     init_win_size: u32,
     max_packet_size: u32,
-    data: Bytes,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct OpenFailure {
     recipient_channel: u32,
     reason_code: u32,
-    description: Bytes,
-    language_tag: Bytes,
+    description: CompactString,
+    language_tag: CompactString,
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -27,14 +27,12 @@ pub(crate) struct BytesAdjust {
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelData {
     recipient_channel: u32,
-    data: Bytes,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelExtendedData {
     recipient_channel: u32,
     data_type: u32,
-    data: Bytes,
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -60,16 +58,14 @@ pub(crate) struct ChannelRequestFailure {
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelRequest {
     recipient_channel: u32,
-    request_type: Bytes,
+    request_type: CompactString,
     want_reply: bool,
-    data: Bytes,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelOpen {
-    channel_type: Bytes,
+    channel_type: CompactString,
     sender_channel: u32,
     init_win_size: u32,
     max_packet_size: u32,
-    data: Bytes,
 }
