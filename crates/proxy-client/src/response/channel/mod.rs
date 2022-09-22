9356bcb35248dffc1aa2739b6a0bd2b7d3192f26
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-23 00:41:15 +1000

    Impl all variants of `Response`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/response/channel/mod.rs
+++ crates/proxy-client/src/response/channel/mod.rs
@@ -1,18 +1,16 @@
-use bytes::Bytes;
 use compact_str::CompactString;
 use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize)]
-pub(crate) struct OpenConfirmation {
-    recipient_channel: u32,
+pub(crate) struct OpenConfirmation<T> {
     sender_channel: u32,
     init_win_size: u32,
     max_packet_size: u32,
+    data: T,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct OpenFailure {
-    recipient_channel: u32,
     reason_code: u32,
     description: CompactString,
     language_tag: CompactString,
@@ -20,46 +18,19 @@ pub(crate) struct OpenFailure {
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct BytesAdjust {
-    recipient_channel: u32,
     bytes_to_add: u32,
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub(crate) struct ChannelData {
-    recipient_channel: u32,
-}
-
-#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelExtendedData {
-    recipient_channel: u32,
     data_type: u32,
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub(crate) struct ChannelEof {
-    recipient_channel: u32,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub(crate) struct ChannelClose {
-    recipient_channel: u32,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub(crate) struct ChannelRequestSuccess {
-    recipient_channel: u32,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub(crate) struct ChannelRequestFailure {
-    recipient_channel: u32,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub(crate) struct ChannelRequest {
-    recipient_channel: u32,
+pub(crate) struct ChannelRequest<T> {
     request_type: CompactString,
     want_reply: bool,
+    data: T,
 }
 
 #[derive(Clone, Debug, Deserialize)]
--- crates/proxy-client/src/response/mod.rs
+++ crates/proxy-client/src/response/mod.rs
@@ -1,6 +1,8 @@
 use bytes::Bytes;
+use ssh_format::{Deserializer, Error};
 
 mod channel;
+pub(crate) use channel::*;
 
 #[derive(Clone, Debug)]
 pub(crate) enum Response {
@@ -10,4 +12,35 @@ pub(crate) enum Response {
         /// Request specific data
         Bytes,
     ),
+
+    ChannelResponse {
+        channel_response_type: ChannelResponseType,
+        recipient_channel: u32,
+        data: Bytes,
+    },
+
+    OpenChannelRequest {
+        header: ChannelOpen,
+        data: Bytes,
+    },
+}
+
+impl Response {
+    pub(crate) fn from_bytes(bytes: Bytes) -> Result<Self, Error> {
+        todo!()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub(crate) enum ChannelResponseType {
+    OpenConfirmation,
+    OpenFailure,
+    BytesAdjust,
+    ChannelData,
+    ChannelExtendedData,
+    ChannelEof,
+    ChannelClose,
+    ChannelRequestSuccess,
+    ChannelRequestFailure,
+    ChannelRequest,
 }
