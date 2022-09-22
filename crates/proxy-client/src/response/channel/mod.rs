c5a30bf366ebdd6201eedf9022fd51de9d6a6d32
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-22 20:36:57 +1000

    Derive `Clone`, `Debug` & `Deserialize` for all channel responses
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/response/channel/mod.rs
+++ crates/proxy-client/src/response/channel/mod.rs
@@ -1,5 +1,7 @@
 use bytes::Bytes;
+use serde::Deserialize;
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct OpenConfirmation {
     recipient_channel: u32,
     sender_channel: u32,
@@ -8,6 +10,7 @@ pub(crate) struct OpenConfirmation {
     data: Bytes,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct OpenFailure {
     recipient_channel: u32,
     reason_code: u32,
@@ -15,38 +18,46 @@ pub(crate) struct OpenFailure {
     language_tag: Bytes,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct BytesAdjust {
     recipient_channel: u32,
     bytes_to_add: u32,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelData {
     recipient_channel: u32,
     data: Bytes,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelExtendedData {
     recipient_channel: u32,
     data_type: u32,
     data: Bytes,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelEof {
     recipient_channel: u32,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelClose {
     recipient_channel: u32,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelRequestSuccess {
     recipient_channel: u32,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelRequestFailure {
     recipient_channel: u32,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelRequest {
     recipient_channel: u32,
     request_type: Bytes,
@@ -54,6 +65,7 @@ pub(crate) struct ChannelRequest {
     data: Bytes,
 }
 
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelOpen {
     channel_type: Bytes,
     sender_channel: u32,
