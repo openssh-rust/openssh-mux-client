13d034ca58fa22e94bebd70fd69dbf7e2635e152
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-23 18:30:31 +1000

    Mark all fields in `response::Channel*` as `pub(crate)`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/response/channel/mod.rs
+++ crates/proxy-client/src/response/channel/mod.rs
@@ -9,33 +9,33 @@ pub(crate) use exit_status::*;
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct OpenConfirmation {
-    sender_channel: u32,
-    init_win_size: u32,
-    max_packet_size: u32,
+    pub(crate) sender_channel: u32,
+    pub(crate) init_win_size: u32,
+    pub(crate) max_packet_size: u32,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct BytesAdjust {
-    bytes_to_add: u32,
+    pub(crate) bytes_to_add: u32,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelExtendedData {
-    data_type: u32,
+    pub(crate) data_type: u32,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelRequest<T> {
-    request_type: CompactString,
-    want_reply: bool,
-    data: T,
+    pub(crate) request_type: CompactString,
+    pub(crate) want_reply: bool,
+    pub(crate) data: T,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub(crate) struct ChannelOpen<T> {
-    channel_type: CompactString,
-    sender_channel: u32,
-    init_win_size: u32,
-    max_packet_size: u32,
-    data: T,
+    pub(crate) channel_type: CompactString,
+    pub(crate) sender_channel: u32,
+    pub(crate) init_win_size: u32,
+    pub(crate) max_packet_size: u32,
+    pub(crate) data: T,
 }
