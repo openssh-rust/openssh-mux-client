454205654fb847cc2919931e56193034c0766b52
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-30 01:00:59 +1000

    Use `Bytes` for `ReceiverWindowSize::extend_window_size_packet`
    since the array cannot be send over `Vec<Bytes>` to the flush task.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/proxy_client/channel/receiver_window_size.rs
+++ crates/proxy-client/src/proxy_client/channel/receiver_window_size.rs
@@ -1,12 +1,14 @@
 use std::sync::atomic::{AtomicU8, Ordering::Relaxed};
 
+use bytes::Bytes;
+
 /// Help maintain receiver window size so that the data/extended_data channel
 /// would not block forever.
 #[derive(Debug)]
 pub(super) struct ReceiverWindowSize {
     /// The packet to sent to expend window size.
     /// It should have all the data required.
-    extend_window_size_packet: [u8; 14],
+    extend_window_size_packet: Bytes,
 
     initial_window_size: u32,
 
@@ -16,7 +18,7 @@ pub(super) struct ReceiverWindowSize {
 
 impl ReceiverWindowSize {
     pub(super) fn new(
-        extend_window_size_packet: [u8; 14],
+        extend_window_size_packet: Bytes,
         initial_window_size: u32,
         opened_spsc_bytes_channel_count: u8,
     ) -> Self {
