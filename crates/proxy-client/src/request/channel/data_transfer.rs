d70836be22402ec5bec7b3e8bf3e5dc4e36035d7
Jiahao XU <Jiahao_XU@outlook.com>
2022-10-05 21:40:33 +1100

    Mod `DataTransfer` to be suitable for zero-copy data transfer
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/request/channel/data_transfer.rs
+++ crates/proxy-client/src/request/channel/data_transfer.rs
@@ -1,9 +1,10 @@
-use std::borrow::Cow;
+use std::convert::TryInto;
 
+use bytes::{Bytes, BytesMut};
 use serde::Serialize;
 
 use super::Request;
-use crate::constants::*;
+use crate::{constants::*, Error};
 
 #[derive(Copy, Clone, Debug, Serialize)]
 pub(crate) struct ChannelAdjustWindow {
@@ -24,19 +25,37 @@ impl ChannelAdjustWindow {
 }
 
 #[derive(Clone, Debug, Serialize)]
-pub(crate) struct DataTransfer<'a> {
+pub(crate) struct DataTransfer {
     recipient_channel: u32,
-    data_transfer: Cow<'a, [u8]>,
+    data_len: u32,
 }
 
-impl<'a> DataTransfer<'a> {
-    pub(crate) fn new(recipient_channel: u32, data_transfer: Cow<'a, [u8]>) -> Request<Self> {
+impl DataTransfer {
+    fn new(recipient_channel: u32, data_len: u32) -> Request<Self> {
         Request::new(
             SSH_MSG_CHANNEL_WINDOW_ADJUST,
             Self {
                 recipient_channel,
-                data_transfer,
+                data_len,
             },
         )
+    }
+
+    /// * `buffer` - This would not modify any existing data in it,
+    ///   but it would create the header on it and split it out as a `Bytes`.
+    pub(crate) fn create_header(
+        &self,
+        recipient_channel: u32,
+        data_len: u32,
+        buffer: &mut BytesMut,
+    ) -> Result<Bytes, Error> {
+        let start = buffer.len();
+
+        Self::new(recipient_channel, data_len).serialize_with_header(buffer, data_len)?;
+
+        // After split_off, buffer contains [0, start), which is the
+        // original content and the returned Bytes contains
+        // [start, capacity), which is the header we just wrote.
+        Ok(buffer.split_off(start).freeze())
     }
 }
