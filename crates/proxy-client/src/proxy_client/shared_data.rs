636a3cabe849b135e45d7476273814b58fc0445d
Jiahao XU <Jiahao_XU@outlook.com>
2022-10-05 19:53:30 +1100

    Add new field `SharedData::channel_data_arena`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/proxy_client/shared_data.rs
+++ crates/proxy-client/src/proxy_client/shared_data.rs
@@ -1,6 +1,13 @@
-use super::channel::MpscBytesChannel;
+use super::channel::{ChannelData, MpscBytesChannel};
+
+const LEN: usize = 64;
+const BITARRAY_LEN: usize = LEN / (usize::BITS as usize);
+
+type ChannelDataArena = concurrent_arena::Arena<ChannelData, BITARRAY_LEN, LEN>;
+pub(super) type ChannelDataArenaArc = concurrent_arena::ArenaArc<ChannelData, BITARRAY_LEN, LEN>;
 
 #[derive(Debug)]
 pub(super) struct SharedData {
     pub(super) write_channel: MpscBytesChannel,
+    pub(super) channel_data_arena: ChannelDataArena,
 }
