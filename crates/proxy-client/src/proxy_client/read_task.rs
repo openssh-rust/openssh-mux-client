e27067ac9b7414beab1564edcecad47bb54fdc7a
Jiahao XU <Jiahao_XU@outlook.com>
2022-10-25 22:04:20 +1100

    Impl handling of open confirmation in read_task
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/proxy_client/read_task.rs
+++ crates/proxy-client/src/proxy_client/read_task.rs
@@ -1,10 +1,17 @@
-use std::{num::NonZeroUsize, pin::Pin};
+use std::{collections::hash_map::Entry, convert::TryInto, num::NonZeroUsize, pin::Pin};
 
+use bytes::BytesMut;
 use hash_hasher::HashedMap as HashMap;
+use ssh_format::from_bytes;
 use tokio::{io::AsyncRead, pin, spawn, task::JoinHandle};
+use tokio_io_utility::read_to_bytes_rng;
 
 use crate::{
-    proxy_client::{ChannelDataArenaArc, SharedData},
+    proxy_client::{
+        channel::{OpenChannelRequestedInner, OpenChannelRes},
+        ChannelDataArenaArc, SharedData,
+    },
+    response::{ChannelResponse, OpenConfirmation, Response},
     Error,
 };
 
@@ -20,7 +27,7 @@ enum PendingRequests {
 
 #[derive(Debug)]
 struct ChannelIngoingData {
-    outgoing_data: ChannelDataArenaArc,
+    outgoing_data_arena_arc: ChannelDataArenaArc,
 
     /// Once this get into zero and `outgoing_data.receivers_count != 0`,
     /// then read task should send `extend_window_size_packet`.
@@ -37,9 +44,81 @@ where
     R: AsyncRead + Send + 'static,
 {
     async fn inner(
-        rx: Pin<&mut (dyn AsyncRead + Send)>,
+        mut rx: Pin<&mut (dyn AsyncRead + Send)>,
         shared_data: SharedData,
     ) -> Result<(), Error> {
+        let mut buffer = BytesMut::with_capacity(1024);
+        let mut ingoing_channel_map: HashMap<u32, ChannelIngoingData> = HashMap::default();
+
+        read_to_bytes_rng(&mut rx, &mut buffer, 4..).await?;
+
+        let packet_len: u32 = from_bytes(&buffer[..4])?.0;
+        let packet_len: usize = packet_len.try_into().unwrap();
+
+        // Excluding the header (`u32`)
+        let packet_bytes_read = buffer.len() - 4;
+
+        if packet_bytes_read < packet_len {
+            read_to_bytes_rng(&mut rx, &mut buffer, (packet_len - packet_bytes_read)..).await?;
+        }
+
+        // Split until (packet_len + 4).
+        // Afterwards, buffer would contain `(packet_len + 4)..`,
+        // and the returned bytes contains``..(packet_len + 4)`.
+        let response = Response::from_bytes(buffer.split_to(packet_len + 4).freeze().slice(4..))?;
+
+        match response {
+            Response::ChannelResponse {
+                channel_response,
+                recipient_channel,
+            } => match channel_response {
+                ChannelResponse::OpenConfirmation(OpenConfirmation {
+                    sender_channel,
+                    init_win_size,
+                    max_packet_size,
+                }) => {
+                    let outgoing_data_arena_arc =
+                        shared_data
+                            .get_channel_data(recipient_channel)
+                            .ok_or(Error::InvalidRecipientChannel(recipient_channel))?;
+
+                    outgoing_data_arena_arc
+                        .sender_window_size
+                        .add(init_win_size.try_into().unwrap());
+
+                    let OpenChannelRequestedInner {
+                        init_receiver_win_size,
+                        extend_window_size_packet,
+                    } = outgoing_data_arena_arc
+                        .state
+                        .set_channel_open_res(OpenChannelRes::Confirmed { max_packet_size })?;
+
+                    let ingoing_data = ChannelIngoingData {
+                        outgoing_data_arena_arc,
+                        receiver_win_size: init_receiver_win_size,
+                        extend_window_size_packet,
+                        pending_requests: PendingRequests::Done,
+                    };
+
+                    match ingoing_channel_map.entry(sender_channel) {
+                        Entry::Occupied(_) => {
+                            return Err(Error::DuplicateSenderChannel(sender_channel));
+                        }
+                        Entry::Vacant(entry) => {
+                            entry.insert(ingoing_data);
+                        }
+                    }
+                }
+                _ => todo!(),
+            },
+            response => {
+                return Err(Error::UnexpectedChannelState {
+                    expected_state: &"ChannelResponse",
+                    actual_state: response.into(),
+                })
+            }
+        }
+
         todo!()
     }
 
