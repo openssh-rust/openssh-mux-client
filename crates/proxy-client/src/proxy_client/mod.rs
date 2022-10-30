f099ec787a1470ad79ec96ef33d4cd05f645ee06
Jiahao XU <Jiahao_XU@outlook.com>
2022-10-30 19:38:31 +1100

    Impl new fn `create_write_task`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/proxy-client/src/proxy_client/mod.rs
+++ crates/proxy-client/src/proxy_client/mod.rs
@@ -8,4 +8,7 @@ use shared_data::{ChannelDataArenaArc, SharedData};
 mod read_task;
 use read_task::create_read_task;
 
+mod write_task;
+use write_task::create_write_task;
+
 pub struct ProxyClient {}
---
+++ crates/proxy-client/src/proxy_client/write_task.rs
@@ -0,0 +1,42 @@
+use std::{num::NonZeroUsize, pin::Pin};
+
+use tokio::{io::AsyncWrite, pin, spawn, task::JoinHandle};
+use tokio_io_utility::{write_all_bytes, ReusableIoSlices};
+
+use crate::{proxy_client::SharedData, Error};
+
+pub(super) async fn create_write_task<W>(
+    tx: W,
+    shared_data: SharedData,
+    reusable_io_slice_cap: NonZeroUsize,
+) -> JoinHandle<Result<(), Error>>
+where
+    W: AsyncWrite + Send + 'static,
+{
+    spawn(async move {
+        pin!(tx);
+
+        create_write_task_inner(tx, shared_data, reusable_io_slice_cap).await
+    })
+}
+
+async fn create_write_task_inner(
+    mut tx: Pin<&mut (dyn AsyncWrite + Send)>,
+    shared_data: SharedData,
+    reusable_io_slice_cap: NonZeroUsize,
+) -> Result<(), Error> {
+    let write_channel = shared_data.get_write_channel();
+    let mut reusable_io_slice = ReusableIoSlices::new(reusable_io_slice_cap);
+
+    let mut buffer = Vec::new();
+
+    loop {
+        write_channel.wait_for_data(&mut buffer).await;
+        if buffer.is_empty() {
+            // Eof
+            break Ok(());
+        }
+
+        write_all_bytes(tx.as_mut(), &mut buffer, &mut reusable_io_slice).await?;
+    }
+}
